ea75510ea5115e1613f886ef4db5d5df46ba6a4a
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2019-07-18 14:22:09 +0530

    Add elements finished

---
+++ day_05/code_snippets/add_elements/script.js
@@ -0,0 +1,28 @@
+// gets executed only when the
+// DOM is ready
+(function() {
+  // not necessary but helps enhance
+  // stability. Dont' worry too much about this
+  "use strict";
+
+  var addHeading = function() {
+    var newDivToAdd = document.createElement("div");
+    // adding relevant CSS class to the div
+    newDivToAdd.classList.add("addable");
+
+    var newHeaderToAdd = document.createElement("h3");
+    var textContent = document.createTextNode("Text added dynamically");
+
+    // adding text to the heading tag
+    newHeaderToAdd.appendChild(textContent);
+    // adding the heading to the div tag
+    newDivToAdd.appendChild(newHeaderToAdd);
+
+    // adding div to the body. This step makes is visible in the DOM
+    document.getElementById("add-target").appendChild(newDivToAdd);
+  };
+
+  // attaching a listener on the button that activates the above
+  // function when clicked
+  document.getElementById("add-button").addEventListener("click", addHeading);
+})();
---
+++ day_05/logistics.md
@@ -0,0 +1,6 @@
+Please do the following before the next class.
+
+- Install Python and PyCharms if not done already
+- Either install Mongo or use mlab
+- Download Postman from [this](https://www.getpostman.com/downloads/) page.
+- Install [Robo3T](https://robomongo.org/download). The free version is sufficient.
