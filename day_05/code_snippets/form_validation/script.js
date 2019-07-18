260d0344907f912fafa940d82a86287f7bfec44e
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2019-07-18 16:44:05 +0530

    Added credits

--- day_05/code_snippets/form_validation/script.js
+++ day_05/code_snippets/form_validation/script.js
@@ -12,6 +12,8 @@ function validateForm(e) {
 
 function hasNumber(textToCheck) {
   for (var i = 0; i < textToCheck.length; i++) {
+    // https://stackoverflow.com/a/8935649/5698202
+    // check if the character is a digit or not
     if ("0123456789".indexOf(textToCheck[i]) !== -1) {
       return true;
     }
